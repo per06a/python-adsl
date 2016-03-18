ca93cefa2a18478c815a8a588b615f4bdfe2c63c
prussell <per@beerdev.net>
2016-03-17 20:15:59 -0700

    Merge, insert sort

--- adsl/sort.py
+++ adsl/sort.py
@@ -83,3 +83,66 @@ def quicksort(array, left=None, right=None):
         quicksort(array, pivot + 1, right)
 
     return array
+
+def merge(llist, rlist):
+    
+    res = []
+    lpos = 0
+    rpos = 0
+    lln = len(llist)
+    rln = len(rlist)
+
+    while lpos < lln or rpos < rln:
+
+        if lpos < lln and rpos < rln:
+            if llist[lpos] <= rlist[rpos]:
+                res.append(llist[lpos])
+                lpos += 1
+
+            else:
+                res.append(rlist[rpos])
+                rpos += 1
+
+        elif lpos < lln:
+            res.append(llist[lpos])
+            lpos += 1
+
+        else:
+            res.append(rlist[rpos])
+            rpos += 1
+
+    return res
+
+def mergesort(llist):
+
+    if len(llist) <= 1:
+        return llist
+
+    pivot = int((0 + len(llist))/2.0)
+    
+    return merge(mergesort(llist[:pivot]),
+                 mergesort(llist[pivot:]))
+
+
+def insertsort(array):
+    # Loop up, loop down
+
+    ln = len(array)
+
+    i = 0
+
+    while i < ln:
+
+        j = ln - 1
+
+        while j > i:
+            if array[j] < array[j-1]:
+                tmp = array[j]
+                array[j] = array[j-1]
+                array[j-1] = tmp
+
+            j -= 1
+        
+        i += 1
+
+    return array
